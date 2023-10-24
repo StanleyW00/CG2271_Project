3cc237fbafaa3997064187f8b05f47e3a2c6371f
elyovs <elyovsun@gmail.com>
2023-10-24 11:24:16 +0800

    change motor to TPM1 to TPM0

--- pwm.c
+++ pwm.c
@@ -2,20 +2,25 @@
 #include "MKL25Z4.h"                  // Device header
 #include "pwm.h"
 
-#define PTB0_Pin 0
-#define PTB1_Pin 1
+//#define PTB0_Pin 0
+//#define PTB1_Pin 1
+//TPM2
 #define PTB2_Pin 2
 #define PTB3_Pin 3
-#define PTC3_Pin 3
-#define PTC4_Pin 4
 
-int volatile modValue1 = 7500;
+//TPM0
+#define PTC2_Pin 2 //CH1
+#define PTC3_Pin 3 //CH2
+#define PTC4_Pin 4 //CH3
+#define PTC8_Pin 8 //CH4
+
+int volatile modValue0 = 7500;
 int volatile modValue2 = 7500;
-int volatile cnvValue1 = 3000;
+int volatile cnvValue0 = 3000;
 int volatile cnvValue2 = 3000;
 
 #define TPM_0 0
-#define TPM_1 1
+//#define TPM_1 1
 #define TPM_2 2
 
 #define LEFT_F 1
@@ -29,11 +34,11 @@ void initPWM() {
 	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
 	
 	// Configure MUX settings
-	PORTB->PCR[PTB0_Pin] &= ~PORT_PCR_MUX_MASK;
-	PORTB->PCR[PTB0_Pin] |= PORT_PCR_MUX(3);
+	//PORTB->PCR[PTB0_Pin] &= ~PORT_PCR_MUX_MASK;
+	//PORTB->PCR[PTB0_Pin] |= PORT_PCR_MUX(3);
 	
-	PORTB->PCR[PTB1_Pin] &= ~PORT_PCR_MUX_MASK;
-	PORTB->PCR[PTB1_Pin] |= PORT_PCR_MUX(3);
+	//PORTB->PCR[PTB1_Pin] &= ~PORT_PCR_MUX_MASK;
+	//PORTB->PCR[PTB1_Pin] |= PORT_PCR_MUX(3);
 	
 	PORTB->PCR[PTB2_Pin] &= ~PORT_PCR_MUX_MASK;
 	PORTB->PCR[PTB2_Pin] |= PORT_PCR_MUX(3);
@@ -47,8 +52,14 @@ void initPWM() {
 	PORTC->PCR[PTC4_Pin] &= ~PORT_PCR_MUX_MASK;
 	PORTC->PCR[PTC4_Pin] |= PORT_PCR_MUX(4);
 	
-	// Enable clock TPM1, TPM2
-	SIM->SCGC6 |= SIM_SCGC6_TPM1_MASK;
+	PORTC->PCR[PTC2_Pin] &= ~PORT_PCR_MUX_MASK;
+	PORTC->PCR[PTC2_Pin] |= PORT_PCR_MUX(4);
+	
+	PORTC->PCR[PTC8_Pin] &= ~PORT_PCR_MUX_MASK;
+	PORTC->PCR[PTC8_Pin] |= PORT_PCR_MUX(3);
+	
+	// Enable clock TPM2
+	//SIM->SCGC6 |= SIM_SCGC6_TPM1_MASK;
 	SIM->SCGC6 |= SIM_SCGC6_TPM2_MASK;
 	
 	// Enable clock TPM0
@@ -59,20 +70,23 @@ void initPWM() {
 	SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1);
 	
 	TPM0->MOD = 7500;
-	TPM1->MOD = 7500;
+	//TPM1->MOD = 7500;
 	TPM2->MOD = 7500;
 	
 	TPM0->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
 	TPM0->SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(7));
 	TPM0->SC &= ~(TPM_SC_CPWMS_MASK);
 	
-	TPM1->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
-	TPM1->SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(7));
-	TPM1->SC &= ~(TPM_SC_CPWMS_MASK);
+	//TPM1->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
+	//TPM1->SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(7));
+	//TPM1->SC &= ~(TPM_SC_CPWMS_MASK);
 	
 	TPM2->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
 	TPM2->SC |= (TPM_SC_CMOD(1) | (TPM_SC_PS(7)));
 	TPM2->SC &= ~(TPM_SC_CPWMS_MASK);
+	
+	TPM0_C1SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM0_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
 	
 	TPM0_C2SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
 	TPM0_C2SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
@@ -80,11 +94,14 @@ void initPWM() {
 	TPM0_C3SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
 	TPM0_C3SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
 	
-	TPM1_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
-	TPM1_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	TPM0_C4SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM0_C4SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
 	
-	TPM1_C1SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
-	TPM1_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	//TPM1_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	//TPM1_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	
+	//TPM1_C1SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	//TPM1_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
 	
 	TPM2_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
 	TPM2_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
@@ -97,13 +114,17 @@ void changeFrequency(int frequency, int timer, int channel) {
 	int mod_val = 375000 / frequency;
 	int cnv_val = mod_val / DUTY_CYCLE;
 	
-	if(timer == TPM_1) {
-		TPM1->MOD = mod_val;
+	if(timer == TPM_0) {
+		TPM0->MOD = mod_val;
 		
-		if(channel == 0) {
-			TPM1_C0V = cnv_val;
-		} else if (channel == 1) {
-			TPM1_C1V = cnv_val;
+		if(channel == 1) {
+			TPM0_C1V = cnv_val;
+		} else if (channel == 2) {
+			TPM0_C2V = cnv_val;
+		} else if (channel == 3) {
+			TPM0_C3V = cnv_val;
+		} else if (channel == 4) {
+			TPM0_C4V = cnv_val;
 		}
 	} else if (timer == TPM_2) {
 		TPM2->MOD = mod_val;
@@ -113,13 +134,5 @@ void changeFrequency(int frequency, int timer, int channel) {
 		} else if (channel == 1) {
 			TPM2_C1V = cnv_val;
 		}
-	} else if (timer == TPM_0) {
-		TPM1->MOD = mod_val;
-		
-		if(channel == 2) {
-			TPM0_C2V = cnv_val;
-		} else if (channel == 3) {
-			TPM0_C3V = cnv_val;
-		}
-	}
+	} 
 }
